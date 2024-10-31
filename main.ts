dd143529a21119b5a5d3565e6ef88f5585983e56
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-31 15:23:26 +0000

    changed the names and added button a

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Oct 31 2024
+ * This program moves pixels
 */
 
-basic.showString('Hello, World!')
+// Setting the varibels
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// Setting the screen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Setting the while loop for button a.
+    input.onButtonPressed(Button.A,function(){
+    loopCounter = 0
+    
+    })

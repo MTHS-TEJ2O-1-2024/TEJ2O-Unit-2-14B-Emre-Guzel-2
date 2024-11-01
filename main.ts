e58c0c2a5bde2de6f94baa0b933a9fafb5cb225f
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-11-01 15:26:58 +0000

    added loop

--- main.ts
+++ main.ts
@@ -14,7 +14,11 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // Setting the while loop for button a.
-    input.onButtonPressed(Button.A,function(){
+ input.onButtonPressed(Button.A,function(){
     loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    // Setting the loop for 
+    while (loopCounter ){
     
-    })
+    }
+})

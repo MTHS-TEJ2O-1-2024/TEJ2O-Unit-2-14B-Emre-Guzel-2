88a269369152c0daa38d4a1538f3811a458db2fe
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-11-04 15:27:16 +0000

    finshe the project

--- main.ts
+++ main.ts
@@ -16,16 +16,16 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // Setting the while loop for button a.
- input.onButtonPressed(Button.A,function(){
-     basic.clearScreen()
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     xcnt = 0
     ycnt = 0
     sprite = game.createSprite(0, 0)
     // Setting the loop for 
-     while (xcnt <= 5){
-        sprite.set(LedSpriteProperty.X ,xcnt)
+    while (xcnt <= 5) {
+        sprite.set(LedSpriteProperty.X, xcnt)
         sprite.set(LedSpriteProperty.Y, ycnt)
-         xcnt++
+        xcnt++
         basic.pause(500)
     }
     basic.pause(500)
@@ -33,38 +33,38 @@ basic.showIcon(IconNames.Happy)
         sprite.set(LedSpriteProperty.X, xcnt)
         sprite.set(LedSpriteProperty.Y, ycnt)
         ycnt++
-         basic.pause(500)
+        basic.pause(500)
     }
     basic.pause(500)
-    while(xcnt>-1){
+    while (xcnt > -1) {
         sprite.set(LedSpriteProperty.X, xcnt)
         sprite.set(LedSpriteProperty.Y, ycnt)
         xcnt--
         basic.pause(500)
     }
-     basic.pause(500)
-    while(ycnt >-1){
+    basic.pause(500)
+    while (ycnt > -1) {
         sprite.set(LedSpriteProperty.X, xcnt)
         sprite.set(LedSpriteProperty.Y, ycnt)
         ycnt--
         basic.pause(500)
     }
     sprite.delete()
-   basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Happy)
 })
 
-input.onButtonPressed(Button.B,function(){
+input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
-     xcnt = 4
-     ycnt = 4
-    sprite = game.createSprite(5,5)
-    while(ycnt>0){
+    xcnt = 4
+    ycnt = 4
+    sprite = game.createSprite(5, 5)
+    while (ycnt > 0) {
         sprite.set(LedSpriteProperty.X, xcnt)
         sprite.set(LedSpriteProperty.Y, ycnt)
         ycnt--
         basic.pause(500)
     }
-    while (xcnt>0) {
+    while (xcnt > 0) {
         sprite.set(LedSpriteProperty.X, xcnt)
         sprite.set(LedSpriteProperty.Y, ycnt)
         xcnt--
